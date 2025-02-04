4f3f51481d193a9151eee4ff8dbe01b32a98cefd
michalsn <michal@sniatala.pl>
2025-02-04 15:17:16 +0100

    fix phpstan

--- src/RunResolver.php
+++ src/RunResolver.php
@@ -207,13 +207,13 @@ class RunResolver
         }
 
         $days = [
-            'sun' => 0,
-            'mon' => 1,
-            'tue' => 2,
-            'wed' => 3,
-            'thu' => 4,
-            'fri' => 5,
-            'sat' => 6,
+            'sun' => '0',
+            'mon' => '1',
+            'tue' => '2',
+            'wed' => '3',
+            'thu' => '4',
+            'fri' => '5',
+            'sat' => '6',
         ];
 
         return str_replace(array_keys($days), array_values($days), $origValue);
